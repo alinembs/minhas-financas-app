626339c09a57edc4ba29a75677641defafc2997a
Aline <alinemariana1@hotmail.com>
2024-02-24 10:36:39 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './views/login';
+import 'bootswatch/dist/pulse/bootstrap.css'
+import './custom.css';
+class App extends React.Component{
+ render(){
+  return(
+ <div>
+   <Login />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ </div>
+
+)}
 }
-
 export default App;
